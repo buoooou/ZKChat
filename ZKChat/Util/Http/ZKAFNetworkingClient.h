b877f01e8c361ef3230d42fc2f77cab7c450e38d
zhangkuo <769329489@qq.com>
2017-04-10 22:38:54 +0800

    AFNetWorking工具类

---
+++ ZKChat/Util/Http/ZKAFNetworkingClient.h
@@ -0,0 +1,17 @@
+//
+//  ZKAFNetworkingClient.h
+//  ZKChat
+//
+//  Created by 张阔 on 2017/4/10.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+
+@interface ZKAFNetworkingClient : NSObject
++(void) jsonFormPOSTRequest:(NSString *)url param:(NSDictionary *)param success:(void (^)(id))success failure:(void (^)(NSError *))failure;
+
++(void) jsonFormRequest:(NSString *)url param:(NSDictionary *)param fromBlock:(void (^)(id <AFMultipartFormData> formData))block success:(void (^)(id))success failure:(void (^)(NSError *))failure;
+
+@end
---
+++ ZKChat/Util/Http/ZKAFNetworkingClient.m
@@ -0,0 +1,16 @@
+//
+//  ZKAFNetworkingClient.m
+//  ZKChat
+//
+//  Created by 张阔 on 2017/4/10.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import "ZKAFNetworkingClient.h"
+
+
+@implementation ZKAFNetworkingClient
+
+
+
+@end
