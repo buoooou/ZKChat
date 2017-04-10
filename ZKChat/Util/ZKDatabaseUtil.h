5acf26ede17f09cf99db5ed9733cc82d1e15c833
zhangkuo <769329489@qq.com>
2017-04-10 22:03:23 +0800

    客户端数据库工具

---
+++ ZKChat/Util/ZKDatabaseUtil.h
@@ -0,0 +1,13 @@
+//
+//  ZKDatabaseUtil.h
+//  ZKChat
+//
+//  Created by 张阔 on 2017/4/10.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZKDatabaseUtil : NSObject
+
+@end
---
+++ ZKChat/Util/ZKDatabaseUtil.m
@@ -0,0 +1,13 @@
+//
+//  ZKDatabaseUtil.m
+//  ZKChat
+//
+//  Created by 张阔 on 2017/4/10.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import "ZKDatabaseUtil.h"
+
+@implementation ZKDatabaseUtil
+
+@end
