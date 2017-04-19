67db11af07f5463afef84dd494e861b20e804f17
zhangkuo <769329489@qq.com>
2017-04-19 22:21:18 +0800

    LoginModule

---
+++ ZKChat/Module/Login/LoginModule.h
@@ -0,0 +1,30 @@
+//
+//  LoginModule.h
+//  ZKChat
+//
+//  Created by 张阔 on 2017/4/19.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ZKUserEntity.h"
+
+
+@interface LoginModule : NSObject
+
+@property (nonatomic,readonly)NSString* token;
++ (instancetype)instance;
+
+/**
+ *  登录接口，整个登录流程
+ *
+ *  @param name     用户名
+ *  @param password 密码
+ */
+- (void)loginWithUsername:(NSString*)name password:(NSString*)password success:(void(^)(ZKUserEntity* user))success failure:(void(^)(NSString* error))failure;
+/**
+ *  离线
+ */
+- (void)offlineCompletion:(void(^)())completion;
+- (void)reloginSuccess:(void(^)())success failure:(void(^)(NSString* error))failure;
+@end
---
+++ ZKChat/Module/Login/LoginModule.m
@@ -0,0 +1,13 @@
+//
+//  LoginModule.m
+//  ZKChat
+//
+//  Created by 张阔 on 2017/4/19.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import "LoginModule.h"
+
+@implementation LoginModule
+
+@end
