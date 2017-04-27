35e870465ae4d5d06e057fefb65765a6e3171dc6
zhangkuo <769329489@qq.com>
2017-04-27 22:13:52 +0800

    头像cell

---
+++ ZKChat/View/Cell/Message/ZKAvatarImageView.h
@@ -0,0 +1,13 @@
+//
+//  ZKAvatarImageView.h
+//  ZKChat
+//
+//  Created by 张阔 on 2017/4/27.
+//  Copyright © 2017年 张阔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZKAvatarImageView : UIImageView
+- (void)setAvatar:(NSString*)avatar group:(BOOL)group;
+@end
